654893b81e0975b86da5fb37b23a9edb302ad73f
Joe Brown <brwnjm@gmail.com>
2012-11-02 10:35:52 -0600

    added job cap
    running_jobs returns only your jobs
    cap limits concurrent jobs in the queue

--- bsub/bsub.py
+++ bsub/bsub.py
@@ -46,7 +46,7 @@ class bsub(object):
 
     @classmethod
     def running_jobs(self):
-        return [x.split()[0] for x in sp.check_output(["bjobs", "-u", "all"]).rstrip().split("\n")[1:]]
+        return [x.split()[0] for x in sp.check_output(["bjobs"]).rstrip().split("\n")[1:]]
 
     @classmethod
     def poll(self, job_ids):
@@ -58,6 +58,15 @@ class bsub(object):
         job_ids = frozenset(job_ids)
         sleep_time = 1
         while job_ids.intersection(self.running_jobs()):
+            time.sleep(sleep_time)
+            if sleep_time < 100:
+                sleep_time += 0.25
+        return True
+
+    @classmethod
+    def cap(self, max_jobs):
+        sleep_time = 1
+        while len(self.running_jobs) >= max_jobs:
             time.sleep(sleep_time)
             if sleep_time < 100:
                 sleep_time += 0.25
